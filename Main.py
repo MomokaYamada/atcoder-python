40b09e0d60da3864be1d6569df9fe16bfd8f7cc1
yamadamotet <60602525+yamadamotet@users.noreply.github.com>
2020-02-16 17:47:15 +0900

    自作ライブラリ

--- Main.py
+++ Main.py
@@ -1,9 +1,2 @@
-text = input()
-
-text = sorted(text)
-
-b = text[0] == text[1] and text[2] == text[3] and text[1] != text[2]
-if b:
-    print("Yes")
-else:
-    print("No")
+def readints():
+    return list(map(int, input().split()))
