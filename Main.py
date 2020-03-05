0db7b0783cb0cf25bc8e14dfc84872a41a49ae5a
yamadamotet <60602525+yamadamotet@users.noreply.github.com>
2020-03-05 15:31:36 +0900

    Add has_duplicates2

--- Main.py
+++ Main.py
@@ -9,3 +9,10 @@ def readints():
 def nCr(n, r):
     return math.factorial(n)//(math.factorial(n-r)*math.factorial(r))
 
+
+def has_duplicates2(seq):  # リストに重複した要素があるか判定（要素にリストがある場合）
+    seen = []
+    for item in seq:
+        if not(item in seen):
+            seen.append(item)
+    return len(seq) != len(seen)
