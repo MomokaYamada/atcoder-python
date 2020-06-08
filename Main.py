cb1a1ab2a234aba9fcfc2466ee482a5f656a166c
yamadamotet <60602525+yamadamotet@users.noreply.github.com>
2020-06-08 13:11:56 +0900

    ABC077 幅優先探索

--- Main.py
+++ Main.py
@@ -35,52 +35,41 @@ def divisor(n):
 # coordinates
 dx = [-1, -1, -1, 0, 0, 1, 1, 1]
 dy = [-1, 0, 1, -1, 1, -1, 0, 1]
-n, m = map(int, input().split())  # 頂点数と辺数
-#print(n, m)
-# グラフ入力受け取り（無効グラフを想定）
-g = [[] for _ in range(n)]
-# print(g)
-for i in range(m):
-    a, b = map(int, input().split())
-    g[a].append(b)
-    g[b].append(a)
-#print('g', g)
-dist = [-1]*n
-# dist[i]はスタート頂点から頂点iまで何ステップで到達できるかを表す
+R, C = map(int, input().split())  # R行C列
+sy, sx = map(int, input().split())  # スタート地点の座標
+gy, gx = map(int, input().split())  # ゴール地点の座標
+c = [None]*R
+# print(c)
+for i in range(R):
+    c[i] = input()
+#print('c', c)
+d = deque()
+d.append((sy-1, sx-1))
+# print(d)
+dist = [-1]*R
+# dist[i][j]には(0,0)から(i,j)までの距離が入る
 # -1で初期化しておく
-d = deque()  # その時点で発見済みだが未訪問な頂点を格納するキュー
-dist[0] = 0  # 初期条件（頂点0を初期ノードとする）
-d.append(0)  # 0は発見済みだが未訪問の頂点
-# BFS開始（dが空っぽになるまで）
+for i in range(R):
+    dist[i] = [-1]*C
+# print(dist)
+dist[sy-1][sx-1] = 0
 while(len(d) != 0):
-    # print(d)
-    v = d.popleft()  # キューから先頭頂点を取り出す
-    # 指定した位置の要素を削除し,値を取得
-    # vから辿れる頂点を全て調べる
-    #print('v', v)
-    #print('g[v]', g[v])
-    for nv in g[v]:
-        # print(nv)
-        if (dist[nv] != -1):  # すでに発見済みの頂点は探索しない
-            continue
-        dist[nv] = dist[v]+1  # 新たな頂点nvについて距離情報を更新してキューに追加する
-        d.append(nv)
-# 各頂点の0からの距離
-for i in range(n):
-    print(i, ':', dist[i])
-
-# 入力
-# 9 13
-# 0 1
-# 0 4
-# 0 2
-# 1 4
-# 1 3
-# 1 8
-# 2 5
-# 3 8
-# 4 8
-# 5 8
-# 5 6
-# 3 7
-# 6 7
+    r, cc = d.popleft()  # 先頭から要素を1つ削除し,その要素を返す
+    #print(r, cc)
+    # print(c[r][cc])
+    if c[r][cc] == '.':
+        if r != 0 and dist[r-1][cc] == -1:  # 上
+            dist[r-1][cc] = dist[r][cc]+1
+            d.append((r-1, cc))
+        if r != R-1 and dist[r+1][cc] == -1:  # 下
+            dist[r+1][cc] = dist[r][cc]+1
+            d.append((r+1, cc))
+        if cc != 0 and dist[r][cc-1] == -1:  # 左
+            dist[r][cc-1] = dist[r][cc]+1
+            d.append((r, cc-1))
+        if cc != C-1 and dist[r][cc+1] == -1:  # 右
+            dist[r][cc+1] = dist[r][cc]+1
+            d.append((r, cc+1))
+    if r == gy-1 and cc == gx-1:
+        print(dist[r][cc])
+        break
